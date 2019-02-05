4756a009e3deb95520595f0b70ec52cff615931c
Tunde Turner <tunde@zerodotdash.com>
2019-02-05 09:55:14 -0600

    refactored code in the stocks controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,15 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: '/users/result' }
-        end
-      else
-        flash[:danger] = 'The stock symbol you\'ve entered cannot be found.'
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = 'You must enter a stock symbol to look up. '
     else
-      flash[:danger] = 'You must enter a stock symbol to look up.'
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = 'The stock symbol you\'ve entered cannot be found. ' unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: '/users/result' }
     end
   end
+
 end
