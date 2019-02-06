3401e39bb89998c65be59da48be2d118ad4659ad
Tunde Turner <tunde@zerodotdash.com>
2019-02-05 19:25:46 -0600

    updated some stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'user/registrations' }
 
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
