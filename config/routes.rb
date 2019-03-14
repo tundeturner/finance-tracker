c7e0d8dc624ab5a03eaf7b47b1492a911c63a4e3
Tunde Turner <tunde@zerodotdash.com>
2019-03-14 08:06:40 -0500

    Added support for friends

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
   get 'price_update', to: 'stocks#update'
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 
   resources :user_stocks, only: [:create, :destroy]
-
+  resources :users, only: [:show]
+  resources :friendships
 end
